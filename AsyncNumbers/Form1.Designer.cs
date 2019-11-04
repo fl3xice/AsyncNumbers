8be9aff1a3c550b9ee3c4db2b48499c890a2b094
fl3xice <two.fl3xice@yandex.ru>
2019-11-04 07:22:46 +0400

    Добавьте файлы проекта.

---
+++ AsyncNumbers/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace AsyncNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.speedMS = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.stoper = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextNumbers = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.speedMS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.speedMS);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.stoper);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.applyButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.TextNumbers);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(569, 270);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label4.Location = new System.Drawing.Point(290, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 9;
+            // 
+            // speedMS
+            // 
+            this.speedMS.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.speedMS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.speedMS.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.speedMS.Location = new System.Drawing.Point(17, 149);
+            this.speedMS.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.speedMS.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.speedMS.Name = "speedMS";
+            this.speedMS.Size = new System.Drawing.Size(540, 23);
+            this.speedMS.TabIndex = 8;
+            this.speedMS.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(14, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Скорость (мс)";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.numericUpDown1.Location = new System.Drawing.Point(17, 103);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(540, 23);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.DoubleClick += new System.EventHandler(this.numbersTextBox_DoubleClick);
+            // 
+            // stoper
+            // 
+            this.stoper.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.stoper.BackColor = System.Drawing.SystemColors.Control;
+            this.stoper.Enabled = false;
+            this.stoper.FlatAppearance.BorderSize = 0;
+            this.stoper.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stoper.Location = new System.Drawing.Point(293, 226);
+            this.stoper.Name = "stoper";
+            this.stoper.Size = new System.Drawing.Size(264, 32);
+            this.stoper.TabIndex = 5;
+            this.stoper.Text = "Стоп";
+            this.stoper.UseVisualStyleBackColor = false;
+            this.stoper.Click += new System.EventHandler(this.StopChangeNumbers);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label2.Location = new System.Drawing.Point(14, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "By vk.com/fl3xice";
+            // 
+            // applyButton
+            // 
+            this.applyButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.applyButton.BackColor = System.Drawing.SystemColors.Control;
+            this.applyButton.FlatAppearance.BorderSize = 0;
+            this.applyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.applyButton.Location = new System.Drawing.Point(17, 226);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(270, 32);
+            this.applyButton.TabIndex = 3;
+            this.applyButton.Text = "Применить";
+            this.applyButton.UseVisualStyleBackColor = false;
+            this.applyButton.Click += new System.EventHandler(this.Apply);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(14, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Число";
+            // 
+            // TextNumbers
+            // 
+            this.TextNumbers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextNumbers.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.TextNumbers.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextNumbers.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TextNumbers.Location = new System.Drawing.Point(12, 9);
+            this.TextNumbers.Name = "TextNumbers";
+            this.TextNumbers.Size = new System.Drawing.Size(545, 62);
+            this.TextNumbers.TabIndex = 0;
+            this.TextNumbers.Text = "0";
+            this.TextNumbers.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 270);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(585, 309);
+            this.Name = "Form1";
+            this.Text = "AsyncNumbers";
+            this.Load += new System.EventHandler(this.FormLoad);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.speedMS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label TextNumbers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button applyButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button stoper;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown speedMS;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AsyncNumbers/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncNumbers
+{
+    public partial class Form1 : Form
+    {
+        decimal Numbers = 0;
+        decimal SettedNumbers = 0;
+        TimeSpan Speed = TimeSpan.FromMilliseconds(1);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void FormLoad(object sender, EventArgs e)
+        {
+            TextNumbers.Text = $"{Numbers}";
+            numericUpDown1.Value = Numbers;
+        }
+
+        async void ChangeNumbers()
+        {
+                while (Numbers != SettedNumbers)
+                {
+                    if (Numbers <= SettedNumbers)
+                    {
+                        await Task.Delay(Speed);
+                        Numbers++;
+                    }
+                    else if (Numbers >= SettedNumbers)
+                    {
+                        await Task.Delay(Speed);
+                        Numbers--;
+                    }
+                    setNumbersText();
+                    
+                }
+                lockAndBlock(true);
+                stoper.Enabled = false;
+                
+        }
+
+        void setNumbersText()
+        {
+            TextNumbers.Text = $"{Numbers}";
+        }
+
+        private void Apply(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value == Numbers)
+            {
+                MessageBox.Show("Введите число которе больше или ниже нынешнего!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                lockAndBlock(false);
+                SettedNumbers = numericUpDown1.Value;
+                Speed = TimeSpan.FromMilliseconds(Convert.ToDouble(speedMS.Value));
+                label4.Text = "Скорость (сек): " + Speed.TotalSeconds;
+                ChangeNumbers();
+                stoper.Enabled = true;
+            }
+            
+        }
+        bool lockAndBlock(bool locked)
+        {
+            applyButton.Enabled = locked;
+            numericUpDown1.Enabled = locked;
+            speedMS.Enabled = locked;
+            return locked ;
+        }
+
+        private void StopChangeNumbers(object sender, EventArgs e)
+        {
+            SettedNumbers = Numbers;
+            stoper.Enabled = false;
+            lockAndBlock(true);
+        }
+
+        private void numbersTextBox_DoubleClick(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 0;
+        }
+    }
+}
